45707a43624e3ef8c9fc5a2e510808245dba1123
Ning Ma <ningma1997@gmail.com>
2020-11-27 10:31:35 +0800

    Initial Commit

---
+++ USTBLogin/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  USTBLogin
+//
+//  Created by Ning Ma on 11/27/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
